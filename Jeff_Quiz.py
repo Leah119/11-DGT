ce0e83084b23e67f41bdc8f8d680fe753daa8213
Leah119 <leahphilip@cromwell.school.nz>
2025-03-19 20:50:26 +1300

    Added list of correct answers

--- Jeff_Quiz.py
+++ Jeff_Quiz.py
@@ -1,4 +1,4 @@
-"""Blah blah, docstring, fix later"""
+"""Blah blah, docstring, fix later""" #actually do or this will be awkward
 
 Ques = ["Who created Amazon?",
         "Who played The Grandmaster?",
@@ -11,10 +11,19 @@ Ques = ["Who created Amazon?",
         "How else can you spell 'Jeffrey'?",
         "Where is the name 'Jeffrey' from?"]
 
+Cor_ans = [["Jeff Bezos", "Jeffrey Bezos", "Bezos"],
+           ["Jeff Goldblum", "Jeffrey Goldblum", "Goldblum"],
+           ["Jeffrey Dahmer", "Jeff Dahmer", "Dahmer"],
+           ["Jeff Buckley", "Jeffrey Buckley", "Buckley"],
+           ["A car", "car", "a car", "racecar", "a racecar"],
+           ["22 Jump Street", "22 jump street"],
+           ["Jeff Bridges", "Jeffrey Bridges", "Bridges"],
+           ["Geoffrey", "Jeffry", "Jefferey", "Jefrie", "Geffrey"],
+           ["Germany", "germany"]]
 
 #Introduction:
 print("Hi there, welcome to the Jeffrey Quiz!")
-start=input("Would you like to begin?\n(Capitals matter)")
+start=input("Would you like to begin?")
 
 if start == "Yes" or start == "yes":
     print("sweet")
